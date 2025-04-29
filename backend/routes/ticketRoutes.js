77ee7d9ece5fd14bcc533fb226b9b6ce57cf6ac9
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-04-29 19:53:36 +0300

    Create ticketRoutes.js

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { createTicket,getTickets } from '../controllers/ticketController.js';
+const router = express.Router();
+
+router.post('/', createTicket);
+router.get('/', getTickets);
+
+export default router;
