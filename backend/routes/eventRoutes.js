434b69580eab26e06e10c077459481157af94b0c
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-04-29 20:00:59 +0300

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
 import { createEvent, getEvents, getEventById } from '../controllers/eventController.js';
-import { protectRoute } from '../middleware/authMiddleware.js';
+//import { protectRoute } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', protectRoute, createEvent);
+router.post('/', createEvent);
 router.get('/', getEvents);
 router.get('/:id', getEventById);
 
