b2ab2b8c25919b28ba8031fc367a20864149e0da
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-05-02 22:05:11 +0300

    Update clubRoutes.js

--- backend/routes/clubRoutes.js
+++ backend/routes/clubRoutes.js
@@ -5,24 +5,26 @@ import {
   getClubById,
   updateClub,
   deleteClub,
+  getClubByName,
 } from "../controllers/clubController.js";
-// import { protectRoute,adminRoute,  adminOrOwnClubRoute,} from "../middleware/authMiddleware.js";
+// import { protectRoute,adminRoute, adminOrOwnClubRoute,} from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
 // Public routes (no auth needed)
 router.get("/", getClubs);
 router.get("/:id", getClubById);
+router.get("/name/:clubName", getClubByName);
 
-
+// Protected routes (admin OR clubadmin can update, but only admin can delete/create)
 
 router.post(
-  "/",
+  "/", // handle file upload
   createClub
-); 
+); // Only Admin can create club
 
-
-router.put("/:id", updateClub); 
-router.delete("/:id", deleteClub); 
+router.put("/:id", updateClub); // Admin and ClubAdmin can update
+router.delete("/:id", deleteClub); // Only Admin can delete
 
 export default router;
