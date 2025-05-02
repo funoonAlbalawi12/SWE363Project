f3e98579347924ba769a7b4061fbff9b20cc93d3
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-05-02 22:00:44 +0300

    Update Club.js

--- backend/models/Club.js
+++ backend/models/Club.js
@@ -1,25 +1,48 @@
-import mongoose from 'mongoose';
-const clubSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
+import mongoose from "mongoose";
+const clubSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    overview: String,
+    vision: String,
+    mission: String,
+    image: {
+      type: String,
+    },
 
-  adminId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
-    required: true, 
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    events: [
+      {
+        title: String,
+        subtitle: String,
+        price: String,
+        date: String,
+        location: String,
+        img: String,
+      },
+    ],
+    socialLinks: [
+      {
+        name: String,
+        icon: String,
+        url: String,
+      },
+    ],
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const Club = mongoose.model('Club', clubSchema);
+const Club = mongoose.model("Club", clubSchema);
 
 export default Club;
