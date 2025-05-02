b1d71f36ac9000f907ea7f47313e66b429deae93
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-05-02 22:12:53 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,40 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
 
-import connectDB from './config/db.js'; 
+import connectDB from "./config/db.js";
 
-
-import userRoutes from './routes/userRoutes.js';
-import eventRoutes from './routes/eventRoutes.js';
-import ticketRoutes from './routes/ticketRoutes.js';
-import clubRoutes from './routes/clubRoutes.js';
-import clubMembershipRoutes from './routes/clubMembershipRoutes.js';
-import eventRegistrationRoutes from './routes/eventRegistrationRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import ticketRoutes from "./routes/ticketRoutes.js";
+import clubRoutes from "./routes/clubRoutes.js";
+import clubMembershipRoutes from "./routes/clubMembershipRoutes.js";
+import eventRegistrationRoutes from "./routes/eventRegistrationRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import joinRequestRoutes from "./routes/joinRequestRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/users/test', (req, res) => {
-  res.status(200).json({ message: 'Test successful!' });
+app.get("/api/users/test", (req, res) => {
+  res.status(200).json({ message: "Test successful!" });
 });
 
-app.use('/api/users', userRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/tickets', ticketRoutes);
-app.use('/api/clubs', clubRoutes);
-app.use('/api/clubmemberships', clubMembershipRoutes);
-app.use('/api/eventregistrations', eventRegistrationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/tickets", ticketRoutes);
+app.use("/api/clubs", clubRoutes);
+app.use("/api/clubmemberships", clubMembershipRoutes);
+app.use("/api/eventregistrations", eventRegistrationRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/join-requests", joinRequestRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
