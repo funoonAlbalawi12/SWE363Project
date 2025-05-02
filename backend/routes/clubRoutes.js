8ca42adfd48e5dc84eef6436dff44c6d9b1f1b03
Khalod3 <Khalodabied@gmail.com>
2025-05-02 23:51:37 +0300

    updates

--- backend/routes/clubRoutes.js
+++ backend/routes/clubRoutes.js
@@ -11,7 +11,6 @@ import {
 } from "../controllers/clubController.js";
 // import { protectRoute,adminRoute, adminOrOwnClubRoute,} from "../middleware/authMiddleware.js";
 
-
 const router = express.Router();
 
 // Configure multer to handle file uploads
@@ -20,7 +19,6 @@ const upload = multer({ dest: "uploads/" }); // You can change this path
 // Create a club with image upload
 router.post("/", upload.single("image"), createClub);
 
-
 // Public routes (no auth needed)
 router.get("/", getClubs);
 router.get("/:id", getClubById);
@@ -28,17 +26,11 @@ router.get("/name/:clubName", getClubByName);
 
 // Protected routes (admin OR clubadmin can update, but only admin can delete/create)
 
-<<<<<<< HEAD
-router.put("/:id", updateClub); 
-router.delete("/:id", deleteClub); 
-=======
+router.put("/:id", updateClub);
+router.delete("/:id", deleteClub);
 router.post(
   "/", // handle file upload
   createClub
 ); // Only Admin can create club
-
-router.put("/:id", updateClub); // Admin and ClubAdmin can update
-router.delete("/:id", deleteClub); // Only Admin can delete
->>>>>>> 30946667a595f5aa1e081e2567a149a69f3ffcba
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-=======
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
->>>>>>> 30946667a595f5aa1e081e2567a149a69f3ffcba
+import path from "path";
+import { fileURLToPath } from "url";
 
 import connectDB from "./config/db.js";
 
@@ -29,7 +22,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 // Get the directory name from the current file URL
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,12 +29,8 @@ const __dirname = path.dirname(__filename);
 // Serve static files from "uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get('/api/users/test', (req, res) => {
-  res.status(200).json({ message: 'Test successful!' });
-=======
 app.get("/api/users/test", (req, res) => {
   res.status(200).json({ message: "Test successful!" });
->>>>>>> 30946667a595f5aa1e081e2567a149a69f3ffcba
 });
 
 app.use("/api/users", userRoutes);
@@ -52,7 +40,6 @@ app.use("/api/clubs", clubRoutes);
 app.use("/api/clubmemberships", clubMembershipRoutes);
 app.use("/api/eventregistrations", eventRegistrationRoutes);
 app.use("/api/payments", paymentRoutes);
-
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
