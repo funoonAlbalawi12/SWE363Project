376a853896ce28acadc6bbe1b3a31667263aea40
Khulud <105064011+KhuludObaid@users.noreply.github.com>
2025-05-03 16:42:08 +0300

    Update clubMembershipRoutes.js

--- backend/routes/clubMembershipRoutes.js
+++ backend/routes/clubMembershipRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { requestMembership, updateMembershipStatus, getMemberships } from '../controllers/clubMembershipController.js';
+import { requestMembership, updateMembershipStatus, getMemberships, getMembershipByUser } from '../controllers/clubMembershipController.js';
 
 const router = express.Router();
 
 router.post('/', requestMembership);
 router.put('/:id', updateMembershipStatus);
 router.get('/', getMemberships);
-
+router.get('/user', getMembershipByUser);
 export default router; 
